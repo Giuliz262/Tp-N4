26c13ced7dfd161353ebc90f8f4b2fecd40f8488
Giuliana Cubilla <giulii024.4@gmail.com>
2024-04-15 14:10:54 -0300

    se agregó botones a cada imagen para ocultar individualmente

--- api/src/components/CaracterCard.jsx
+++ api/src/components/CaracterCard.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CaracterCard({ imageUrl }) {
+  const [isVisible, setIsVisible] = useState(true); 
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', width: '300px' }}>
-      <img src={imageUrl} alt="Pug" style={{ maxWidth: '100%' }} />
+      {isVisible && <img src={imageUrl} alt="Pug" style={{ maxWidth: '100%' }} />} 
+      {isVisible && <button onClick={toggleVisibility}>Ocultar</button>}
     </div>
   );
 }
